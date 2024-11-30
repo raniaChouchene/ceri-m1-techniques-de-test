d2340e2cc43a4bc41e304fc4eca1a975c53722e2
Rania Chouchene <rania.chouchene.2019@gmail.com>
2024-11-30 19:25:54 +0100

    Fix Checkstyle whitespace violations

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -4,9 +4,12 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-        if(name!="" && team!=null && pokedexFactory!=null) {
-            IPokedex pokedex=pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory(new PokemonMetadataProvider()));
-            return new PokemonTrainer(name,team,pokedex);
+        if (name != null && !name.isEmpty() && team != null && pokedexFactory != null) {
+            IPokedex pokedex = pokedexFactory.createPokedex(
+                    new PokemonMetadataProvider(),
+                    new PokemonFactory(new PokemonMetadataProvider())
+            );
+            return new PokemonTrainer(name, team, pokedex);
         }
         return null;
     }
